66cbeb9540dc9d6a814d2e0a8bff33be8d0dda12
Angel Sangerman <a_armando97@hotmail.com>
2023-07-03 20:20:41 -0600

    Removed commented code

--- src/App/index.js
+++ src/App/index.js
@@ -3,14 +3,6 @@ import { AppUI } from './AppUI';
 import { TodoProvider } from '../TodoContext';
 /* import './App.css'; */
 
-/* const defaultTodos = [
-   {text: 'Aprender React', completed: false},
-   {text: 'Llorar con la llorona', completed: true},
-   {text: 'Deploy react', completed: false},
-   {text: 'Build React App', completed: false},
-] */
-
-
 
 function App() {
  
